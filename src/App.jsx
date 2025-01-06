95677e7c0c24fc17a0da6704b0810bd0fc00096e
vaibhav kottari <kottarivaibhav@gmail.com>
2025-01-06 19:00:02 +0530

    main files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import Pricing from './sections/Pricing'
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+       <Header />
+       <Hero />
+       <Features />
+       <Pricing />
+    </main>
+  )
+}
+
+export default App
