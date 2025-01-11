e7ef1f36021e1a0cb4fa721fc39f01f6b2e98f55
vaibhav kottari <kottarivaibhav@gmail.com>
2025-01-11 09:50:13 +0530

    source file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Header from './sections/Header'
 import Hero from './sections/Hero'
 import Features from './sections/Features'
 import Pricing from './sections/Pricing'
+import Faq from './sections/Faq'
+import Testimonials from './sections/Testimonials'
+import Download from './sections/Download'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
@@ -11,6 +15,10 @@ const App = () => {
        <Hero />
        <Features />
        <Pricing />
+       <Faq />
+       <Testimonials />
+       <Download />
+       <Footer />
     </main>
   )
 }
